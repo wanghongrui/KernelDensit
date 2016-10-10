2263300590b6eb0191a68cce9e60dcd116f1b2fb
wanghongrui <wanghongruigood@qq.com>
2016-10-10 15:56:14 +0800

    add grouping function

--- KernelDensit.py
+++ KernelDensit.py
@@ -30,19 +30,20 @@ arcpy.AddMessage("Finished extract points...")
 # 2.2 读取公共点
 arcpy.AddMessage("2.2 Read common points...")
 commonpointdict = dict()
-with arcpy.da.SearchCursor("dh_common", ["OID@", "ORIG_FID", "FID_dh_explode", "SHAPE@X", "SHAPE@Y"]) as cursor:
+with arcpy.da.SearchCursor("dh_common", ["OID@", "FID_dh_explode", "SHAPE@X", "SHAPE@Y"]) as cursor:
     for row in cursor:
         cpoint = dict()
-        cpoint["ORIG_FID"] = row[1]
-        cpoint["FID_dh_explode"] = row[2]
-        cpoint["X"] = round(row[3], 2)
-        cpoint["Y"] = round(row[4], 2)
+        cpoint["FID_dh_explode"] = row[1]
+        cpoint["X"] = round(row[2], 2)
+        cpoint["Y"] = round(row[3], 2)
+        cpoint["commonid"] = 0
+        cpoint["groupid"] = 0
         commonpointdict[row[0]] = cpoint
 arcpy.AddMessage("Finished read common points...")
 
 # 2.3 逐条读取线
 arcpy.AddMessage("2.3 Read road lines...")
-linerows = arcpy.da.SearchCursor("dh_explode",("OID@", "SHAPE@", "SHAPE@LENGTH", "ORIG_FID"))
+linerows = arcpy.da.SearchCursor("dh_explode",("OID@", "SHAPE@", "SHAPE@LENGTH"))
 originlinedict = dict()
 for row in linerows:
     origin = dict()
@@ -52,12 +53,64 @@ for row in linerows:
             points.append([round(pnt.X, 2), round(pnt.Y, 2)])
     origin["length"] = round(row[2], 2)
     origin["points"] = points
-    origin["ORIG_FID"] = row[3]
-    origin["group"] = 0
+    origin["commonid"] = 0
     originlinedict[row[0]] = origin
 arcpy.AddMessage("Finished read road lines...")
 
 # 2.4 依据连通性进行分组
 arcpy.AddMessage("2.4 Grouping according to common point...")
+'''
+Tips. Intersect工具算出来的公共点dh_common，
+如果dh_common属性中两个点的FID_dh_explode有相同的值x，
+那么dh_explode中OID = x 表示的折线，就是联通这两个点的线，
+那么我们就给他一个相同的groupid，表示两者是“相通”的。
+'''
+repeatfiddict = dict()
+commonid_index = 1
+for k, v in commonpointdict.iteritems():
+    if repeatfiddict.has_key(v["FID_dh_explode"]) <> True:
+        repeatfiddict[v["FID_dh_explode"]] = k
+    else:
+        if commonpointdict[repeatfiddict[v["FID_dh_explode"]]]["commonid"] <> 0:
+            v["commonid"] = commonpointdict[repeatfiddict[v["FID_dh_explode"]]]["commonid"]
+        else:
+            commonpointdict[repeatfiddict[v["FID_dh_explode"]]]["commonid"] = commonid_index
+            v["commonid"] = commonid_index
+            commonid_index = commonid_index + 1
+
+tempdict = dict()
+for k, v in commonpointdict.iteritems():
+    if tempdict.has_key(str(v["X"])+str(v["Y"])) <> True:
+        tempdict[str(v["X"])+str(v["Y"])] = []
+    if v["commonid"] <> 0 and v["commonid"] not in tempdict[str(v["X"])+str(v["Y"])]:
+        tempdict[str(v["X"])+str(v["Y"])].append(v["commonid"])
+
+commondict = dict()
+for k, v in tempdict.iteritems():
+    if v <> False:
+        for i in v:
+            if commondict.has_key(i):
+                tempdict[k] = list(set(v).union(set(tempdict[commondict[i]])))
+                tempdict[commondict[i]] = tempdict[k]
+            else:
+                commondict[i] = k
+
+groupid_index = 1000000
+for k, v in commonpointdict.iteritems():
+    if v["commonid"] in tempdict[str(v["X"]) + str(v["Y"])] or tempdict.has_key(str(v["X"]) + str(v["Y"])):
+        _groupid = ''.join(map(str, tempdict[str(v["X"]) + str(v["Y"])]))
+        if _groupid <> "":
+            v["groupid"] = int(_groupid)
+        else:
+            v["groupid"] = groupid_index
+            groupid_index = groupid_index + 1
+
+arcpy.AddMessage("Finished grouping points...")
+
+'''
+要不要对commonpointdict进行压缩，去掉冗余的数据。
+对于这个问题，明天再做思考2016年10月10日15:55:25
+'''
+
 
 
