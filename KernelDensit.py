d5568d82e9923b92d373460a83e8c68fa6aa7bb6
wanghongrui <wanghongruigood@qq.com>
2016-10-19 07:54:06 +0800

    去除groupid，接着往下做

--- KernelDensit.py
+++ KernelDensit.py
@@ -8,10 +8,11 @@ gPopulationLayer = arcpy.GetParameterAsText(1)
 gPopulationField = arcpy.GetParameterAsText(2)
 gSupermarketLayer = arcpy.GetParameterAsText(3)
 gSplitLength = arcpy.GetParameterAsText(4)
+gH = arcpy.GetParameterAsText(5)
 
 gDesc = arcpy.Describe(gRoadLayer)
 gPath = gDesc.path;
-
+'''
 # 1. 预处理
 arcpy.AddMessage("1. Pretreatment...")
 # 1.1 融合数据
@@ -37,97 +38,6 @@ arcpy.Delete_management("dh_common")
 arcpy.Intersect_analysis("dh_explode", "dh_common", "", "", "POINT")
 arcpy.AddMessage("Finished extract points...")
 
-# 2.2 读取公共点
-arcpy.AddMessage("2.2 Read common points...")
-commonpointdict = dict()
-with arcpy.da.SearchCursor("dh_common", ["OID@", "FID_dh_explode", "SHAPE@X", "SHAPE@Y"]) as cursor:
-    for row in cursor:
-        cpoint = dict()
-        cpoint["FID_dh_explode"] = row[1]
-        cpoint["X"] = round(row[2], 2)
-        cpoint["Y"] = round(row[3], 2)
-        cpoint["commonid"] = 0
-        cpoint["groupid"] = 0
-        commonpointdict[row[0]] = cpoint
-arcpy.AddMessage("Finished read common points...")
-
-# 2.3 依据连通性进行分组
-arcpy.AddMessage("2.3 Grouping according to common point...")
-'''
-Tips. Intersect工具算出来的公共点dh_common，
-如果dh_common属性中两个点的FID_dh_explode有相同的值x，
-那么dh_explode中OID = x 表示的折线，就是联通这两个点的线，
-那么我们就给他一个相同的groupid，表示两者是“相通”的。
-'''
-repeatfiddict = dict()
-commonid_index = 1
-for k, v in commonpointdict.iteritems():
-    if repeatfiddict.has_key(v["FID_dh_explode"]) <> True:
-        repeatfiddict[v["FID_dh_explode"]] = k
-    else:
-        if commonpointdict[repeatfiddict[v["FID_dh_explode"]]]["commonid"] <> 0:
-            v["commonid"] = commonpointdict[repeatfiddict[v["FID_dh_explode"]]]["commonid"]
-        else:
-            commonpointdict[repeatfiddict[v["FID_dh_explode"]]]["commonid"] = commonid_index
-            v["commonid"] = commonid_index
-            commonid_index = commonid_index + 1
-
-tempdict = dict()
-for k, v in commonpointdict.iteritems():
-    if tempdict.has_key(str(v["X"])+str(v["Y"])) <> True:
-        tempdict[str(v["X"])+str(v["Y"])] = []
-    if v["commonid"] <> 0 and v["commonid"] not in tempdict[str(v["X"])+str(v["Y"])]:
-        tempdict[str(v["X"])+str(v["Y"])].append(v["commonid"])
-
-commondict = dict()
-for k, v in tempdict.iteritems():
-    if v <> False:
-        for i in v:
-            if commondict.has_key(i):
-                tempdict[k] = list(set(v).union(set(tempdict[commondict[i]])))
-                tempdict[commondict[i]] = tempdict[k]
-            else:
-                commondict[i] = k
-
-max_groupid = 0
-temp_groupid = 0
-tempkeyvaluedict = dict()
-for k, v in commonpointdict.iteritems():
-    if tempkeyvaluedict.has_key(str(v["X"]) + str(v["Y"])) <> True:
-        tempkeyvaluedict[str(v["X"]) + str(v["Y"])] = []
-    if v["commonid"] in tempdict[str(v["X"]) + str(v["Y"])] or tempdict.has_key(str(v["X"]) + str(v["Y"])):
-        if tempdict[str(v["X"]) + str(v["Y"])]:
-            temp_groupid = tempdict[str(v["X"]) + str(v["Y"])][0]  # 以列表中第一个元素，作为groupid
-            if temp_groupid > max_groupid:
-                max_groupid = temp_groupid
-            v["groupid"] = temp_groupid
-
-arcpy.AddMessage("Finished grouping points...")
-
-
-'''
-#要不要对commonpointdict进行压缩，去掉冗余的数据。
-#对于这个问题，明天再做思考2016年10月10日15:55:25
-'''
-# 2.4 压缩groupid
-arcpy.AddMessage("2.5 Compass groupid...")
-groupid_index = max_groupid + 1
-groupidpointdict = dict()
-for k, v in commonpointdict.iteritems():
-    if groupidpointdict.has_key(str(v["X"]) + str(v["Y"])) == False:
-        if v["groupid"]:
-            groupidpointdict[str(v["X"]) + str(v["Y"])] = v["groupid"]
-        else:
-            groupidpointdict[str(v["X"]) + str(v["Y"])] = groupid_index
-            groupid_index = groupid_index + 1
-        
-arcpy.AddMessage(groupidpointdict)
-
-'''
-#Tip. 存在潜在的问题：当很多条线段连接同一个点时，这个点的groupid就太大了。
-#    因为设计的就是这么狗血groupid:1125781257912580125771258412586125911221312215478479
-#    这里有待于优化，我先用本方法试着改过来2016年10月16日21:57:17
-'''
 
 # 2.5 逐条读取线,并剖分
 arcpy.AddMessage("2.5 Read road lines and split it...")
@@ -152,16 +62,6 @@ for row in linerows:
         for pnt in part:
             points.append((round(pnt.X, 2), round(pnt.Y, 2)))
 
-    if groupidpointdict.has_key(str(points[0][0])+str(points[0][1])):
-        origin["groupid"] = groupidpointdict[str(points[0][0])+str(points[0][1])]
-    elif groupidpointdict.has_key(str(points[len(points) - 1][0]) + str(points[len(points) - 1][1])):
-        origin["groupid"] = groupidpointdict[str(points[len(points) - 1][0]) + str(points[len(points) - 1][1])]
-    else:
-        origin["groupid"] = groupid_index
-        groupid_index = groupid_index + 1
-        #
-        #    #Tips: 3W多条数据，大概有90条线段没有找到groupid——2016年10月17日13:54:58
-        #
 
     origin["length"] = round(row[2], 2)
   
@@ -216,9 +116,9 @@ arcpy.Delete_management("dh_split")
 arcpy.Delete_management("dh_split.shp")
 dh_split = arcpy.CreateFeatureclass_management(gDesc.path, "dh_split", "POLYLINE", "", "DISABLED", "DISABLED", "dh_explode")
 fc_fields = (   
- ("id", "LONG", None, None, None, "", "NON_NULLABLE", "REQUIRED"),  
- ("length", "FLOAT", None, None, None, "", "NULLABLE", "NON_REQUIRED"),  
- ("groupid", "TEXT", None, None, 256, "", "NULLABLE", "NON_REQUIRED")
+ ("population", "LONG", None, None, None, "", "NON_NULLABLE", "REQUIRED"),  # 人口
+ ("generator", "SHORT", None, None, None, "", "NON_NULLABLE", "REQUIRED"),  # 发生元，超市
+ ("length", "FLOAT", None, None, None, "", "NULLABLE", "NON_REQUIRED")      # 剖分后的长度
  ) 
 
 for fc_field in fc_fields:  
@@ -226,19 +126,12 @@ for fc_field in fc_fields:
 
 arcpy.AddMessage(dh_split)
 
-with arcpy.da.InsertCursor('dh_split', ["id", "length", "groupid", "SHAPE@"]) as inscur:
+with arcpy.da.InsertCursor('dh_split', ["length", "SHAPE@"]) as inscur:
     for k, v in segdict.iteritems():
         seg_point = arcpy.Array()
         for point in v["points"]:
             seg_point.add(arcpy.Point(point[0], point[1]))
-        #arcpy.AddMessage(v)
-        fc_groupid = str(v["groupid"])
-        #
-         #报错： v["groupid"]取不到值，也就是说这条线段没有获取到groupid。2016年10月16日22:50:45
-        #
-        if fc_groupid > 49:
-            fc_groupid = fc_groupid[:49]
-        inscur.insertRow((k, v["length"], fc_groupid, arcpy.Polyline(seg_point)))
+        inscur.insertRow((v["length"], arcpy.Polyline(seg_point)))
 del inscur
 
 arcpy.AddMessage("Generated split layers...")
@@ -252,8 +145,61 @@ arcpy.Near_analysis(gPopulationLayer, dh_split, "200 Meters", "NO_LOCATION", "NO
 arcpy.Near_analysis(gSupermarketLayer, dh_split, "200 Meters", "NO_LOCATION", "NO_ANGLE")
 
 arcpy.AddMessage(arcpy.GetMessages())
+'''
+popdict = dict()
+superdict = dict()
+
+'''
+ 这里，我们使用popdict来存储人口，superdict描述超市信息，这个"dict"应该不会很大，
+ 不会超过population和supermarket的总和：
+ 用法： 以"NEAR_FID"作为key，其值则为数组，一个标识人口，第二个为超市；
+ 注意的是，人口数是叠加的
+'''
+arcpy.AddMessage(gPopulationField)
+with arcpy.da.SearchCursor(gPopulationLayer, ("NEAR_FID", gPopulationField)) as rpopcur:
+    for pcur in rpopcur:
+        if pcur[0] <> -1:
+            if popdict.has_key(pcur[0]):
+                popdict[pcur[0]] = popdict[pcur[0]] + pcur[1]
+            else:
+                popdict[pcur[0]] = pcur[1]
+arcpy.AddMessage(popdict)
 
 
+with arcpy.da.SearchCursor(gSupermarketLayer, ("NEAR_FID")) as rsupcur:
+    for scur in rsupcur:
+        if pcur[0] <> -1:
+            superdict[scur[0]] = 1
 
+with arcpy.da.UpdateCursor('dh_split', ['OID@', "population", "generator"]) as updcur:
+    for ucur in updcur:
+        if popdict.has_key(ucur[0]):
+            ucur[1] = popdict[ucur[0]]
+        if superdict.has_key(ucur[0]):
+            ucur[2] = superdict[ucur[0]]
+        updcur.updateRow(ucur) 
 
+del rpopcur, rsupcur, updcur
+
+# 缓冲，筛选出generator以及split——2016年10月18日22:03:49
+arcpy.Delete_management("dh_generators")
+arcpy.Select_analysis("dh_split", "dh_generators", "generator = 1")
+
+arcpy.Delete_management("dh_spatialjoin")
+arcpy.SpatialJoin_analysis("dh_split", "dh_generators", "dh_spatialjoin", "JOIN_ONE_TO_MANY", "KEEP_COMMON", "", "WITHIN_A_DISTANCE", gH + " Meters")
+
+# 计算
+'''
+将数据从dh_spatialjoin中全部取出。
+以x，y坐标作为key。
+然后将以坐标来查找split及后续的population
+'''
+linedict = dict()
+pointdict = dict()
+with arcpy.da.SearchCursor("dh_spatialjoin", ("OID@", "population", "generator", "SHAPE@", "SHAPE@LENGTH")) as rsegcur:
+    for segcur in rsegcur:
+        origin = dict()
+        for part in segcur[3]:
+            
+            
 
